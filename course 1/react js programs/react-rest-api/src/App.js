19ca389fbf3924ae91dd8592f8a02528d9e08186
akash <akash300383@gmail.com>
2024-05-26 22:34:04 +0530

    done

---
+++ course 1/react js programs/react-rest-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FetchDemo from './FetchDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Rest API using axios and fetch</h2>
+      <FetchDemo></FetchDemo>
+    </div>
+  );
+}
+
+export default App;
