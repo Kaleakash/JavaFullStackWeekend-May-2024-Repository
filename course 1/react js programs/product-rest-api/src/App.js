5311571cc6bf69aaffe25758562bd8cfe3992397
akash <akash300383@gmail.com>
2024-06-01 21:31:49 +0530

    done

--- course 1/react js programs/product-rest-api/src/App.js
+++ course 1/react js programs/product-rest-api/src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import DisplayProduct from './DisplayProduct';
 import AddProduct from './AddProduct';
 import DeleteProduct from './DeleteProduct';
+import UpdateProduct from './UpdateProduct';
 
 function App() {
   return (
     <div className="App">
       <h2>Product Management System Using json-server with axios</h2>
       <AddProduct></AddProduct>
+      <hr/>
       <DeleteProduct></DeleteProduct>
+      <hr/>
+      <UpdateProduct></UpdateProduct>
+      <hr/>
       <DisplayProduct></DisplayProduct>
     </div>
   );
