cfdd3a777b15055cdd6e84cfdfa28536b5d141d6
akash <akash300383@gmail.com>
2024-06-01 19:54:50 +0530

    done

---
+++ course 1/react js programs/product-rest-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayProduct from './DisplayProduct';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Product Management System Using json-server with axios</h2>
+      <DisplayProduct></DisplayProduct>
+    </div>
+  );
+}
+
+export default App;
