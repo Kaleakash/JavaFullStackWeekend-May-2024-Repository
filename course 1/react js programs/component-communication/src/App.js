0b75ca39d2536fd4bc15bd04f787af844f8540d5
akash <akash300383@gmail.com>
2024-06-01 22:47:34 +0530

    done

--- course 1/react js programs/component-communication/src/App.js
+++ course 1/react js programs/component-communication/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Parent from './Parent';
+import Student from './Student';
 
 function App() {
+  //  single line 
+  /*
+  multi line 
+  */
   return (
     <div className="App">
-     <Parent></Parent>
+     {/* <Parent></Parent> */}
+     <hr/>
+     <Student></Student>
     </div>
   );
 }
