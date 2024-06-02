8033a689c0cdd15689a5d87e20fe29e70bb34462
akash <akash300383@gmail.com>
2024-06-02 21:45:59 +0530

    done

---
+++ course 1/react js programs/react-with-redux/src/reducer.js
@@ -0,0 +1,14 @@
+let initialState = {
+    name:"Akash"        // global state variable. 
+}
+
+
+function reducer(state=initialState,action) {
+
+    // we can do some action 
+
+    return state;
+}
+
+export default reducer;
+
