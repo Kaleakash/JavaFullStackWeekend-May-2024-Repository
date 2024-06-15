ad8f6dad99ef3bfde9b62f7893d05e45665b6e0d
akash <akash300383@gmail.com>
2024-06-15 22:09:46 +0530

    done

---
+++ course 2/Angular Projects/welcome-app/src/app/template-reference/template-reference.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-reference',
+  templateUrl: './template-reference.component.html',
+  styleUrls: ['./template-reference.component.css']
+})
+export class TemplateReferenceComponent {
+  msg:string="";
+  passValue(nameRef:any): void {
+    let name = nameRef.value;
+    alert("welcome user "+name)
+  }
+
+  checkUser(a:any,b:any):void {
+    let emailid = a.value;
+    let passowrd = b.value;
+   
+    if(emailid=="akash@gmail.com" && passowrd=="123"){
+        this.msg="success"
+    }else {
+        this.msg="failure"
+    }
+
+    a.value=""    // we want reset value in text field 
+    b.value=""
+  }
+}
