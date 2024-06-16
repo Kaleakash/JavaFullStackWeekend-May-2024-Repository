3f6760196d4c8a2fd2c51e9320bb7c99a17e5af6
akash <akash300383@gmail.com>
2024-06-16 19:28:11 +0530

    done

--- course 2/Angular Projects/welcome-app/src/app/mdf-login/mdf-login.component.ts
+++ course 2/Angular Projects/welcome-app/src/app/mdf-login/mdf-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mdf-login',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./mdf-login.component.css']
 })
 export class MdfLoginComponent {
+
+loginRef = new FormGroup({
+  emailid:new FormControl(),
+  password:new FormControl()
+});
+
+msg:string ="";
+checkUser():void {
+  //alert("event fired")
+  let login = this.loginRef.value;
+  if(login.emailid=="akash@gmail.com" && login.password=="123"){
+      this.msg="successfully login"
+  }else {
+      this.msg="failure try once again"
+  }
+  this.loginRef.reset();
+}
 
 }
