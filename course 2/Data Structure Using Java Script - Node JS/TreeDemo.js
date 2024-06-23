3c0c81ab07ece5b44be48856ed4eb105ae007336
akash <akash300383@gmail.com>
2024-06-23 22:11:16 +0530

    done

--- course 2/Data Structure Using Java Script - Node JS/TreeDemo.js
+++ course 2/Data Structure Using Java Script - Node JS/TreeDemo.js
@@ -8,6 +8,7 @@ class Node {
 class BinaryTree {
     constructor(){
         this.root = null;       // root is null 
+
     }
     insert(data){
         let newNode = new Node(data);   //100
@@ -16,10 +17,23 @@ class BinaryTree {
             this.root=newNode;
             console.log("root node created..")
         }else {
-            console.log("logic to write the node may be left or right")
+            //console.log("logic to write the node may be left or right")
+            this.insertNode(this.root,newNode);
         }
+    }
+
+    insertNode(node,newNode){
+        if(node.left==null){
+            node.left=newNode;
+            console.log("added left of root node")
+        }else {
+            console.log("added right of root node")
+            node.right=newNode;
+        }   
     }
 }
 let tree = new BinaryTree();
 tree.insert(100);
 tree.insert(10);
+tree.insert(50);
+console.log(tree);
