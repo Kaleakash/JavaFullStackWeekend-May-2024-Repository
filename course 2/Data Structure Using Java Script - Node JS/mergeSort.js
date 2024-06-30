7f2a67c7e2c63acec72460fb4f600cdfe7a4fe25
akash <akash300383@gmail.com>
2024-06-30 19:27:46 +0530

    done

--- course 2/Data Structure Using Java Script - Node JS/mergeSort.js
+++ course 2/Data Structure Using Java Script - Node JS/mergeSort.js
@@ -1,4 +1,4 @@
-let unsortedData = [4,1,7,2,5,9,8,3];
+let unsortedData = [4,1,7,2,5,9,8,3,56,34,90,45];
 console.log("unsorted data "+unsortedData);
 //mergeSort(unsortedData);
 let sortedData = mergeSort(unsortedData);
@@ -11,13 +11,14 @@ function mergeSort(arr){
     }else {
    let mid = Math.floor(arr.length / 2);      // 4
    let leftHalf = arr.slice(0,mid)  // 0 to 3 position 4-1 2nd parameter size-1
-   console.log("mid "+mid)
    let rightHalf =  arr.slice(mid)  
-   //console.log(leftHalf+"="+rightHalf)
+   console.log("mid "+mid)
+   console.log(leftHalf+"="+rightHalf)
    // recursively sort each halves 
    let sortedLeftHalf = mergeSort(leftHalf);
    let sortedRightHalf = mergeSort(rightHalf);
-	
+   console.log(sortedLeftHalf)
+   console.log(sortedRightHalf)	
    let result = merge(sortedLeftHalf,sortedRightHalf);
     	return result;
 	}
@@ -30,7 +31,7 @@ function merge(leftArray,rightArray){
 
     // merge left and right into result in sorted order. 
     while(leftIndex < leftArray.length && rightIndex < rightArray.length){
-        
+            console.log("leftIndex "+leftIndex+" Right Index "+rightIndex);
             if(leftArray[leftIndex] > rightArray[rightIndex]){
                 result.push(leftArray[leftIndex])
                 leftIndex++;
@@ -38,6 +39,7 @@ function merge(leftArray,rightArray){
                 result.push(rightArray[rightIndex])
                 rightIndex++;
             }
+            console.log("LeftArray "+leftArray+"Right Array "+rightArray)
     }
 
     let remainingData = result.concat(leftArray.slice(leftIndex)).concat(rightArray.slice(rightIndex));
