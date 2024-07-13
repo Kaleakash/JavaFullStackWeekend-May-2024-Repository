ddc991ce5f62af1e7ed010d62d4c30b5bbb19d0e
akash <akash300383@gmail.com>
2024-07-13 20:59:25 +0530

    done

--- course 3/Core Java Projects/WelcomeApp/src/DataTypes.java
+++ course 3/Core Java Projects/WelcomeApp/src/DataTypes.java
@@ -6,11 +6,13 @@ public class DataTypes {
 		double c=100.40;
 		char d = '$';
 		boolean result = true;
+		String name="Akash";
 		//System.out.println(a);
 		System.out.println(b);
 		System.out.println("Value of b is "+b);
 		System.out.println("Value of c is "+c);
 		System.out.println("Value of d is "+d);
 		System.out.println("Value of result is "+result);
+		System.out.println("name is "+name);
 	}
 }
