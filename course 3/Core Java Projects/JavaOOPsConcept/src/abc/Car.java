5c03fb5bb23219ace695423189b7cf2d1c580262
akash <akash300383@gmail.com>
2024-07-14 20:06:42 +0530

    done

--- course 3/Core Java Projects/JavaOOPsConcept/src/abc/Car.java
+++ course 3/Core Java Projects/JavaOOPsConcept/src/abc/Car.java
@@ -13,7 +13,25 @@ public class Car {
 		price = 1000000;			// default value set for all object whenever memory created
 	}
 	
+	Car(int wheel, String colour,float price) {			// parameterized constructor 
+		System.out.println("object created with parameter");
+		this.wheel=wheel;
+		this.colour=colour;
+		this.price=price;			// parameterized constructor 
+	}
 	// behaviour 
+	
+//	void setCarInfo(int wheel1, String colour1, float price1) {		// it is use to set the to instance variable. 
+//		wheel=wheel1;
+//		colour=colour1;
+//		price=price1;
+//	}
+	
+	void setCarInfo(int wheel, String colour, float price) {		// it is use to set the to instance variable. 
+		this.wheel=wheel;			// instance variable and local or parameter variable have same 
+		this.colour=colour;			// then local variable hide the visibility of instance variable 
+		this.price=price;			// to avoid, we need to use this.instance variable. 
+	}
 	void start() {
 		System.out.println("car start");
 	}
--- course 3/Core Java Projects/JavaOOPsConcept/src/abc/CarTest.java
+++ course 3/Core Java Projects/JavaOOPsConcept/src/abc/CarTest.java
@@ -16,13 +16,26 @@ public class CarTest {
 		innova.price=2600000;
 		innova.wheel=4;
 		innova.carInfo("Innova Car");
+		
 		System.out.println("-----------------------");
+		
 		Car ertiga = new Car();
 		ertiga.carInfo("Ertiga Car");
 		ertiga.colour="Gray";
 		ertiga.price=1500000;
 		ertiga.wheel=4;
 		ertiga.carInfo("Ertiga Car");
+		
+		System.out.println("-----------------------");
+		
+		Car santro = new Car();
+		santro.setCarInfo(4, "RED", 900000);		// passing value using parameter concept. 
+		santro.carInfo("Santro");
+		
+		System.out.println("-----------------------");
+		
+		Car nexon = new Car(4,"Blue",1250000);
+		nexon.carInfo("Nexon");
 	}
 
 }
