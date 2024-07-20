33f6c090bb2232b02f36dc40692185a2bc73016c
akash <akash300383@gmail.com>
2024-07-20 20:55:53 +0530

    done

---
+++ course 3/Core Java Projects/JavaOOPsConcept/src/finalkeyword/FinalKeywordExample.java
@@ -0,0 +1,25 @@
+package finalkeyword;
+final class Bike {
+	final void speed() {
+		System.out.println("60km/hr");
+	}
+}
+//class Honda extends Bike {
+////	@Override
+////	void speed() {
+////		System.out.println("90km/hr");
+////	}
+//}
+public class FinalKeywordExample {
+
+	public static void main(String[] args) {
+		final int A=10;
+		//A=20;// error 
+		System.out.println(A);
+	//	Honda hh = new Honda();
+	//	hh.speed();
+		Bike bb = new Bike();
+		bb.speed();
+	}
+
+}
