995da79e92ed9b2ff14b71699787cd847ecb8e9b
akash <akash300383@gmail.com>
2024-07-20 20:21:34 +0530

    done

--- course 3/Core Java Projects/JavaOOPsConcept/src/polymorphsim/MethodOverriding.java
+++ course 3/Core Java Projects/JavaOOPsConcept/src/polymorphsim/MethodOverriding.java
@@ -4,12 +4,12 @@ class Bike {
 		System.out.println("60km/hr");
 	}
 }
-class Honda extends Bike {
+class Honda extends Bike {				// re-usability : using super class speed method 
 	void color() {
 		System.out.println("Black");
 	}
 }
-class Pulsar extends Bike {
+class Pulsar extends Bike {			// overriding : speed method 
 	void color() {
 		System.out.println("Gray");
 	}
@@ -18,12 +18,22 @@ class Pulsar extends Bike {
 		System.out.println("90km/hr");
 	}
 }
+class Tvs extends Bike {			// merge both the super and sub class method. 
+	void color() {
+		System.out.println("RED");
+	}
+	@Override
+	void speed() {
+		super.speed();     // calling super class speed method : ie 60 
+		System.out.println("10km/hr");			// + 10 merge code. 
+	}
+}
 public class MethodOverriding {
 	public static void main(String[] args) {
 		Bike bb = new Bike();		bb.speed();   
 		Honda hh = new Honda(); 	hh.speed();     hh.color();
 		Pulsar pu = new Pulsar();	pu.speed();     pu.color();
-
+		Tvs tvs = new Tvs();		tvs.speed();    tvs.color();
 	}
 
 }
