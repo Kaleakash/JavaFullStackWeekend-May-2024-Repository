a4c47f82bdf884f0c63c298de61146e67aef063e
akash <akash300383@gmail.com>
2024-07-21 20:35:07 +0530

    done

--- course 3/Core Java Projects/ExceptionHandlingApp/src/com/TryAndCatchBlock.java
+++ course 3/Core Java Projects/ExceptionHandlingApp/src/com/TryAndCatchBlock.java
@@ -5,10 +5,16 @@ public class TryAndCatchBlock {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		int a=10;
-		int b=0;
+		int b=1;
+		int abc[]= {10,20,30,40};
+		String str = "10abc";
 		try {
 		int res = a/b;
 		System.out.println("REsult is "+res);
+		int res1 = abc[2];
+		System.out.println("result1 is "+res1);
+		System.out.println(str+10);   // 1010
+		System.out.println(Integer.parseInt(str)+10);// 20 
 		}catch(Exception e) {
 			//System.out.println("I Take Care!");
 			//System.out.println("message "+e.getMessage());
