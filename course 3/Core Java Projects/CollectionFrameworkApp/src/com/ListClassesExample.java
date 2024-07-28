05acedff4a7431be60ca2e9395943a445fdb10e2
akash <akash300383@gmail.com>
2024-07-28 21:27:03 +0530

    done

--- course 3/Core Java Projects/CollectionFrameworkApp/src/com/ListClassesExample.java
+++ course 3/Core Java Projects/CollectionFrameworkApp/src/com/ListClassesExample.java
@@ -3,6 +3,7 @@ package com;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
 
 public class ListClassesExample {
 
@@ -28,28 +29,35 @@ public class ListClassesExample {
 //		System.out.println("search "+ll1.contains(100));
 		
 		// LinkedList 
-		LinkedList ll1 =new LinkedList();
-		ll1.add(10);
-		ll1.add(30);
-		ll1.add(50);
-		ll1.add(20);
-		ll1.add(80);
-		System.out.println(ll1);
-		System.out.println("find value using index "+ll1.get(1));
-		ll1.add(1, 100);
-		System.out.println(ll1);
-		System.out.println("find value using index "+ll1.get(1));
-		ll1.remove(2); 	// remove using index 
-		Integer i = 50;
-		ll1.remove(i);		// remove using index 
-		System.out.println(ll1);
-		System.out.println("search "+ll1.contains(10));
-		System.out.println("search "+ll1.contains(100));
-		System.out.println("first element "+ll1.getFirst());
-		System.out.println("last element "+ll1.getLast());
-		ll1.removeFirst();
-		ll1.removeLast();
-		System.out.println(ll1);
+//		LinkedList ll1 =new LinkedList();
+//		ll1.add(10);
+//		ll1.add(30);
+//		ll1.add(50);
+//		ll1.add(20);
+//		ll1.add(80);
+//		System.out.println(ll1);
+//		System.out.println("find value using index "+ll1.get(1));
+//		ll1.add(1, 100);
+//		System.out.println(ll1);
+//		System.out.println("find value using index "+ll1.get(1));
+//		ll1.remove(2); 	// remove using index 
+//		Integer i = 50;
+//		ll1.remove(i);		// remove using index 
+//		System.out.println(ll1);
+//		System.out.println("search "+ll1.contains(10));
+//		System.out.println("search "+ll1.contains(100));
+//		System.out.println("first element "+ll1.getFirst());
+//		System.out.println("last element "+ll1.getLast());
+//		ll1.removeFirst();
+//		ll1.removeLast();
+//		System.out.println(ll1);
+		// Vector 
+		Vector ll = new Vector();
+		ll.add(10);						// part of list method 	
+		ll.addElement(20);				// part of vector method 
+		System.out.println(ll);
+		
+		
 	}
 
 }
