3973371b5bc19a6160e99d1f1b518887221aca21
akash <akash300383@gmail.com>
2024-07-28 21:54:37 +0530

    done

--- course 3/Core Java Projects/CollectionFrameworkApp/src/com/ListClassesExample.java
+++ course 3/Core Java Projects/CollectionFrameworkApp/src/com/ListClassesExample.java
@@ -3,6 +3,7 @@ package com;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 import java.util.Vector;
 
 public class ListClassesExample {
@@ -52,11 +53,23 @@ public class ListClassesExample {
 //		ll1.removeLast();
 //		System.out.println(ll1);
 		// Vector 
-		Vector ll = new Vector();
-		ll.add(10);						// part of list method 	
-		ll.addElement(20);				// part of vector method 
-		System.out.println(ll);
+//		Vector ll = new Vector();
+//		ll.add(10);						// part of list method 	
+//		ll.addElement(20);				// part of vector method 
+//		System.out.println(ll);
 		
+		// Stack Operation
+		Stack ss = new Stack();
+		ss.push(100);
+		ss.push(200);
+		ss.push(300);
+		System.out.println(ss);
+		System.out.println("Pop "+ss.pop());		// remove top element 
+		System.out.println(ss);
+		System.out.println("Peek "+ss.peek());		// it display top element 
+		System.out.println(ss);
+		System.out.println("Search "+ss.search(100));
+		System.out.println("Search "+ss.search(1000));
 		
 	}
 
