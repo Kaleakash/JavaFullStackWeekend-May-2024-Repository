6ff4165283be946c3a9fdfaf302355a160314611
akash <akash300383@gmail.com>
2024-08-03 23:06:58 +0530

    done

--- course 3/Core Java Projects/JdbcTestApp/src/com/JdbcTestDemo.java
+++ course 3/Core Java Projects/JdbcTestApp/src/com/JdbcTestDemo.java
@@ -8,6 +8,15 @@ public class JdbcTestDemo {
 		System.out.println("Driver loaded successfully");
 Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "root@123");
 System.out.println("Connected successfully");
+	Statement stmt = con.createStatement();
+	
+		int result = stmt.executeUpdate("insert into employee values(5,'Neena',27000)");
+		if(result>0) {
+			System.out.println("Record inserted succesfully");
+		}
+		
+		stmt.close();
+		con.close();
 		} catch (Exception e) {
 			System.err.println(e);
 		}
