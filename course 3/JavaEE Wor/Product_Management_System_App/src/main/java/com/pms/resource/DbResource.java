749a38ac182094c67f7b198eda702cce6a60de2a
akash <akash300383@gmail.com>
2024-08-17 23:21:33 +0530

    done

--- course 3/JavaEE Wor/Product_Management_System_App/src/main/java/com/pms/resource/DbResource.java
+++ course 3/JavaEE Wor/Product_Management_System_App/src/main/java/com/pms/resource/DbResource.java
@@ -5,16 +5,25 @@ import java.sql.DriverManager;
 
 public class DbResource {
 
-	
+static Connection con;
 	public static Connection getDbConnection() {
 		
 		try {
 	Class.forName("com.mysql.cj.jdbc.Driver");
-Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "root@123");
-return con;
+con = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root", "root@123");
+	return con;
 		} catch (Exception e) {
 			System.err.println(e);
 		}
 		return null;
+	}
+	
+	
+	public static void close() {
+		try {
+			con.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 }
