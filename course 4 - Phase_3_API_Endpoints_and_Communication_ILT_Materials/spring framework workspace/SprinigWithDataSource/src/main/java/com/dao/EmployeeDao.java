047bafa04c09f78bdff62152b62af4b889b4f207
Akash <akash300383@gmail.com>
2024-09-14 20:06:35 +0530

    done

--- course 4 - Phase_3_API_Endpoints_and_Communication_ILT_Materials/spring framework workspace/SprinigWithDataSource/src/main/java/com/dao/EmployeeDao.java
+++ course 4 - Phase_3_API_Endpoints_and_Communication_ILT_Materials/spring framework workspace/SprinigWithDataSource/src/main/java/com/dao/EmployeeDao.java
@@ -3,6 +3,7 @@ package com.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.bean.Employee;
@@ -18,8 +20,8 @@ import com.bean.Employee;
 @Repository						// singleton 
 public class EmployeeDao {
 	
-	@Autowired	// autowire search database connection using byType in xml file. 
-	DataSource ds;// before spring framework we were doing configuration for data source in 
+	//@Autowired	// autowire search database connection using byType in xml file. 
+	//DataSource ds;// before spring framework we were doing configuration for data source in 
 			// application server like web logic, jboss etc. and using jndi lookup we were searching 
 				// database connection. 
 	
@@ -92,23 +94,35 @@ return jdbcTemplate.update("update employee set salary = ? where id = ?", emp.ge
 		}
 	}
 	
+//	public List<Employee> findAll() {
+//		List<Employee> listofemp = new ArrayList<Employee>();
+//		try {
+//			Connection con = ds.getConnection();
+//			PreparedStatement pstmt = con.prepareStatement("select * from employee");
+//			ResultSet rs = pstmt.executeQuery();
+//			while(rs.next()) {
+//				Employee emp = new Employee();
+//				emp.setId(rs.getInt(1));
+//				emp.setName(rs.getString(2));
+//				emp.setSalary(rs.getFloat(3));
+//				listofemp.add(emp);
+//			}
+//		} catch (Exception e) {
+//			System.err.println(e);
+//		}
+//		return listofemp;
+//	}
+	
+	// query method : 1st parameter query and 2nd parameter RowMapper interface reference 
+	// Rowmap
 	public List<Employee> findAll() {
-		List<Employee> listofemp = new ArrayList<Employee>();
 		try {
-			Connection con = ds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement("select * from employee");
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				Employee emp = new Employee();
-				emp.setId(rs.getInt(1));
-				emp.setName(rs.getString(2));
-				emp.setSalary(rs.getFloat(3));
-				listofemp.add(emp);
-			}
+return jdbcTemplate.query("select * from employee", new MyRowMapper());	
 		} catch (Exception e) {
 			System.err.println(e);
+			return null;
 		}
-		return listofemp;
+	
 	}
 	
 	public List<Map<String, Object>> findAllAsListOfMap() {	// each record retrieve as a map object. 
@@ -118,5 +132,16 @@ return jdbcTemplate.update("update employee set salary = ? where id = ?", emp.ge
 			System.err.println(e);
 			return null;
 		}
+	}
+}
+// Global while loop. 
+class MyRowMapper implements RowMapper<Employee>{
+	@Override
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Employee emp = new Employee();
+		emp.setId(rs.getInt(1));
+		emp.setName(rs.getString(2));
+		emp.setSalary(rs.getFloat(3));
+		return emp;
 	}
 }
